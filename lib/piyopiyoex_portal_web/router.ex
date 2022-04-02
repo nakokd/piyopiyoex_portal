058680addb01566a34f37416b099c8b4daeeb473
ymn <ae86bg5@yahoo.co.jp>
2022-04-02 12:56:41 +0900

    /をmessagesにする #1

--- lib/piyopiyoex_portal_web/router.ex
+++ lib/piyopiyoex_portal_web/router.ex
@@ -17,14 +17,12 @@ defmodule PiyopiyoexPortalWeb.Router do
   scope "/", PiyopiyoexPortalWeb do
     pipe_through :browser
 
-    live "/messages", MessageLive.Index, :index
+    live "/", MessageLive.Index, :index
     live "/messages/new", MessageLive.Index, :new
     live "/messages/:id/edit", MessageLive.Index, :edit
 
     live "/messages/:id", MessageLive.Show, :show
     live "/messages/:id/show/edit", MessageLive.Show, :edit
-
-    get "/", PageController, :index
   end
 
   # Other scopes may use custom stacks.
