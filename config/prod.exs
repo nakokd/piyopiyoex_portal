9b5538f46146612de2050aa78f9bffce6a65df3c
kn339264 <kanapo.kd@gmail.com>
2022-06-04 17:02:40 +0900

    change deploy app

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,8 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :piyopiyoex_portal, PiyopiyoexPortalWeb.Endpoint,
-  url: [host: "piyopiyoex-test.gigalixirapp.com"],
-  check_origin: ["//piyopiyoex-test.gigalixirapp.com"],
+  url: [host: "piyopiyoex-portal.gigalixirapp.com"],
+  check_origin: ["//piyopiyoex-portal.gigalixirapp.com"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
