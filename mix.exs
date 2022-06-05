9beba88db80602ad44ff7f0607739290beab7fa7
kn339264 <kanapo.kd@gmail.com>
2022-06-05 13:18:28 +0900

    mod assets.deploy

--- mix.exs
+++ mix.exs
@@ -65,7 +65,7 @@ defmodule PiyopiyoexPortal.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["cmd --cd assets npm run deploy", "esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["esbuild default --minify", "phx.digest", "cmd --cd assets npm run deploy"]
     ]
   end
 end
